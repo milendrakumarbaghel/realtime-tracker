93abd2b9fb78dc1ca707c740bd7e20c82eee217d
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-01-14 22:40:56 +0530

    successfully setup socetio and all cdn's

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const dotenv = require('dotenv');
+const path = require('path'); // Importing the path module
+
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('hey!');
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); // Correct usage of path
+
+io.on('connection', function(socket) {
+    console.log("connected");
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
