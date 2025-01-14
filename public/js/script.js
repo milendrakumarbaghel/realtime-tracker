8ebfbec3ba2039a60420e8a6eb214d066c6295d6
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-01-14 23:30:21 +0530

    completed, successfully setup recieve-location & user-disconnection

--- public/js/script.js
+++ public/js/script.js
@@ -13,14 +13,28 @@ if(navigator.geolocation) {
     });
 }
 
-const map = L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 15);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "Spy Map"
 }).addTo(map);
 
-const markerss = {};
+const markers = {};
 
 socket.on("recieve-location", (data) => {
     const { id, latitude, longitude } = data;
-    map.setView([latitude, longitude], 1);
+    map.setView([latitude, longitude]);
+
+    if(markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+})
+
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
 })
